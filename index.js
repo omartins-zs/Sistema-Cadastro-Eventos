8cdbdf718aacc93f1c92355df5c57155299a5cc7
Gabriel Martins <gabrielmatheus209@gmail.com>
2021-09-01 21:26:44 -0300

    Third request

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const nomeEvento = "Festa do Branco";
 
+console.log(nomeEvento + "\n");
 
+// Third request Palestrantes
 
+let listaDeParticipantes = ["Helena", " Chico ", " Mário", " José", " Maria"];
+
+let listaDePalestrante = [
+  "Andrea Aparecida Novaes",
+  "Nicolas Lorenzo Thales Aragão",
+  "Otávio Henrique Souza",
+];
+
+let quantidadeDePalestrante = listaDePalestrante.length;
+console.log("Estes são nosso palestrantes :" + "\n");
+for (let indice = 0; indice < quantidadeDePalestrante; indice++) {
+  const PalestranteDoEvento = listaDePalestrante[indice];
+  console.log(PalestranteDoEvento);
+}
+//
